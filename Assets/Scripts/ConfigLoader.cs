176335227008621bf8752fe90cdaa92d12d143a1
Антон Таранов <antontaranov@Mac-mini-Anton.local>
2020-02-14 18:04:55 +0300

    Initial commit

---
+++ Assets/Scripts/ConfigLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+using CirclesWar.Data;
+
+namespace CirclesWar
+{
+    public class ConfigLoader : MonoBehaviour
+    {
+		private void Awake()
+		{
+            var asset = Resources.Load<TextAsset>("GameConfig");
+
+            var configMap = JsonUtility.FromJson<ConfigMap>(asset.text);
+
+            if (configMap != null)
+            {
+                var gameConfig = configMap.GameConfig;
+                Debug.Log("config load success");           
+            }
+            else
+            {
+                Debug.LogError("config load error");
+            }
+		}
+
+        [Serializable]
+        class ConfigMap
+        {
+            public GameConfig GameConfig;
+        }
+	}
+}
---
+++ Assets/Scripts/Data/GameConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace CirclesWar.Data
+{
+    [Serializable]
+    public class GameConfig
+    {
+        public float gameAreaWidth;
+        public float gameAreaHeight;
+        public int numUnitsToSpawn;
+        public float unitSpawnDelay;
+        public float unitSpawnMinRadius;
+        public float unitSpawnMaxRadius;
+        public float unitSpawnMinSpeed;
+        public float unitSpawnMaxSpeed;
+        public float unitDestroyRadius;
+    }
+}
